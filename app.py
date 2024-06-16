88ff8939248b39bfbe9bd1e37c0fdbad72d6989a
ItsZekken <jperaltac@uninorte.edu.co>
2024-06-15 23:20:05 -0500

    esta sí e

--- app.py
+++ app.py
@@ -18,17 +18,6 @@ def analyze_sentiment(note):
     analysis = TextBlob(note)
     return analysis.sentiment.polarity
 
-def unify_tag_category(tag):
-    if isinstance(tag, dict):
-        tag_type = tag.get('type', '').lower()
-        if tag_type in ['emociones', 'emotions']:
-            return 'Emociones'
-        elif tag_type in ['actividades', 'activities']:
-            return 'Actividades'
-        else:
-            return tag.get('type', None)
-    return None
-
 def main():
     st.title("Análisis de Datos de Pixels")
 
@@ -69,26 +58,37 @@ def main():
         tags_df['tag_entries'] = tags_df['tags'].apply(lambda x: x['entries'] if isinstance(x, dict) else [])
         tags_exploded = tags_df.explode('tag_entries')
 
-        # Unificar categorías de tags en español e inglés
-        tags_exploded['tags'] = tags_exploded['tags'].apply(lambda x: {'type': unify_tag_category(x), 'entries': x['entries']} if isinstance(x, dict) else x)
-        tag_categories = tags_exploded['tags'].apply(lambda x: x['type'] if isinstance(x, dict) else None).unique()
+        tag_categories = tags_df['tags'].apply(lambda x: x['type'] if isinstance(x, dict) else None).unique()
 
         combined_tags_df = main_df.copy()
 
+        # Definir mapeo de nombres de categorías
+        category_mapping = {
+            'Actividades': ['Actividades', 'Activities'],
+            'Emociones': ['Emociones', 'Emotions']
+        }
+
         for category in tag_categories:
-            category_df = tags_exploded[tags_exploded['tags'].apply(lambda x: x['type'] if isinstance(x, dict) else None) == category]
-            category_count = category_df['tag_entries'].value_counts().reset_index()
-            category_count.columns = ['Tag', 'Frecuencia']
+            matched_category = None
+            for key, values in category_mapping.items():
+                if category in values:
+                    matched_category = key
+                    break
 
-            st.write(f"Frecuencia de {category}")
-            fig_category = px.bar(category_count, x='Tag', y='Frecuencia', title=f'Frecuencia de {category}')
-            fig_category.update_layout(template='plotly_dark', xaxis_title=category, yaxis_title='Frecuencia')
-            fig_category.update_traces(marker_color='khaki')
-            st.plotly_chart(fig_category, use_container_width=True)
+            if matched_category:
+                category_df = tags_exploded[tags_exploded['tags'].apply(lambda x: x['type'] if isinstance(x, dict) else None) == matched_category]
+                category_count = category_df['tag_entries'].value_counts().reset_index()
+                category_count.columns = ['Tag', 'Frecuencia']
 
-            category_df['flag'] = 1
-            pivot_df = category_df.pivot_table(index='date', columns='tag_entries', values='flag', fill_value=0)
-            combined_tags_df = pd.merge(combined_tags_df, pivot_df, on='date', how='left').fillna(0)
+                st.write(f"Frecuencia de {matched_category}")
+                fig_category = px.bar(category_count, x='Tag', y='Frecuencia', title=f'Frecuencia de {matched_category}')
+                fig_category.update_layout(template='plotly_dark', xaxis_title=matched_category, yaxis_title='Frecuencia')
+                fig_category.update_traces(marker_color='khaki')
+                st.plotly_chart(fig_category, use_container_width=True)
+
+                category_df['flag'] = 1
+                pivot_df = category_df.pivot_table(index='date', columns='tag_entries', values='flag', fill_value=0)
+                combined_tags_df = pd.merge(combined_tags_df, pivot_df, on='date', how='left').fillna(0)
 
         # Correlación entre todas las categorías de tags y el puntaje
         st.subheader("Correlación entre Tags y Puntaje")
@@ -125,11 +125,59 @@ def main():
         fig_sentiment.update_traces(marker_color='khaki')  # Cambia el color de las líneas del gráfico de sentimiento a 'khaki'
         st.plotly_chart(fig_sentiment, use_container_width=True)
 
+        # Correlaciones más importantes entre actividades y emociones
+        st.subheader("Correlaciones entre Actividades y Emociones")
+        
+        # Filtrar actividades y emociones
+        activities_df = tags_exploded[tags_exploded['tags'].apply(lambda x: x['type'] if isinstance(x, dict) else None).isin(category_mapping['Actividades'])]
+        emotions_df = tags_exploded[tags_exploded['tags'].apply(lambda x: x['type'] if isinstance(x, dict) else None).isin(category_mapping['Emociones'])]
+        
+        activities_df['activity_flag'] = 1
+        emotions_df['emotion_flag'] = 1
+
+        pivot_activities_df = activities_df.pivot_table(index='date', columns='tag_entries', values='activity_flag', fill_value=0)
+        pivot_emotions_df = emotions_df.pivot_table(index='date', columns='tag_entries', values='emotion_flag', fill_value=0)
+
+        combined_activities_emotions_df = pd.merge(pivot_activities_df, pivot_emotions_df, on='date', how='left').fillna(0)
+        
+        corr_activities_emotions = combined_activities_emotions_df.corr()
+        
+        # Filtrar correlaciones entre actividades y emociones
+        activity_emotion_corr = corr_activities_emotions.loc[pivot_activities_df.columns, pivot_emotions_df.columns]
+        
+        # Selección de actividad
+        selected_activity = st.selectbox("Selecciona una Actividad", pivot_activities_df.columns)
+
+        # Obtener correlaciones de la actividad seleccionada
+        selected_corr = activity_emotion_corr.loc[selected_activity]
+
+        # Calcular umbral para seleccionar las correlaciones más altas (positivas y negativas)
+        threshold = selected_corr.abs().quantile(0.75)  # Usando el percentil 75 como umbral
+        significant_corr = selected_corr[(selected_corr.abs() >= threshold)].sort_values()
+
+        # Gráfico de barras de las correlaciones
+        st.write(f"Correlaciones significativas para {selected_activity}")
+        fig_corr_bars = go.Figure()
+        fig_corr_bars.add_trace(go.Bar(
+            x=significant_corr,
+            y=significant_corr.index,
+            orientation='h',
+            marker=dict(color=significant_corr.apply(lambda x: '#f0e68c' if x > 0 else '#54382a'))
+        ))
+        fig_corr_bars.update_layout(
+            template='plotly_dark',
+            title=f'Correlaciones de {selected_activity} con Emociones',
+            xaxis_title='Correlación',
+            yaxis_title='Emoción',
+            xaxis=dict(range=[-1, 1])
+        )
+        st.plotly_chart(fig_corr_bars, use_container_width=True)
+
         # Modelo predictivo para puntaje de estado de ánimo
         st.subheader("Modelo Predictivo para Puntaje de Estado de Ánimo")
         
         # Preparar datos para el modelo
-        X = combined_tags_df.drop(columns=['date', 'score', 'notes', 'rolling_avg_score', 'sentiment'])
+        X = combined_tags_df.drop(columns=['date', 'score', 'notes', 'sentiment'])
         y = combined_tags_df['score']
 
         # Dividir los datos en conjunto de entrenamiento y prueba
@@ -152,67 +200,6 @@ def main():
         fig_pred.add_trace(go.Scatter(x=[0, 5], y=[0, 5], mode='lines', line=dict(color='red', dash='dash')))
         fig_pred.update_layout(template='plotly_dark', xaxis_title='Valores Reales', yaxis_title='Predicciones')
         st.plotly_chart(fig_pred, use_container_width=True)
-
-        # Correlaciones más importantes entre actividades y emociones
-        st.subheader("Correlaciones entre Actividades y Emociones")
-        
-        # Filtrar actividades y emociones
-        activities_df = tags_exploded[tags_exploded['tags'].apply(lambda x: unify_tag_category(x) == 'Actividades')]
-        emotions_df = tags_exploded[tags_exploded['tags'].apply(lambda x: unify_tag_category(x) == 'Emociones')]
-        
-        activities_df['activity_flag'] = 1
-        emotions_df['emotion_flag'] = 1
-
-        pivot_activities_df = activities_df.pivot_table(index='date', columns='tag_entries', values='activity_flag', fill_value=0)
-        pivot_emotions_df = emotions_df.pivot_table(index='date', columns='tag_entries', values='emotion_flag', fill_value=0)
-
-        combined_activities_emotions_df = pd.merge(pivot_activities_df, pivot_emotions_df, on='date', how='left').fillna(0)
-
-        # Calcular las correlaciones entre actividades y emociones
-        corr_activities_emotions = combined_activities_emotions_df.corr()
-
-        # Preparar estructuras para almacenar los resultados
-        activities = activities_df['tag_entries'].unique()
-        correlations = {}
-
-        # Calcular umbral dinámico para selección de correlaciones significativas
-        threshold = corr_activities_emotions.abs().mean().mean()
-
-        # Filtrar y ordenar las correlaciones significativas para cada actividad
-        for activity in activities:
-            correlations[activity] = corr_activities_emotions.loc[activity].drop(activity).abs().nlargest(5)
-
-        # Graficar las correlaciones más altas para cada actividad
-        st.subheader("Correlaciones más Altas entre Actividades y Emociones")
-
-        for activity, top_correlations in correlations.items():
-            if len(top_correlations) > 0:
-                fig_corr_activity = go.Figure()
-
-                # Preparar datos para las barras
-                x_values = top_correlations.index
-                y_values = top_correlations.values
-                colors = ['green' if corr > 0 else 'red' for corr in y_values]
-
-                fig_corr_activity.add_trace(go.Bar(
-                    x=x_values,
-                    y=y_values,
-                    marker_color=colors,
-                    orientation='v',
-                    text=[f'{corr:.2f}' for corr in y_values],
-                    textposition='outside'
-                ))
-
-                fig_corr_activity.update_layout(
-                    title=f'Correlaciones para la Actividad: {activity}',
-                    xaxis_title='Emoción',
-                    yaxis_title='Correlación',
-                    template='plotly_dark',
-                    yaxis=dict(tickmode='linear', tick0=0, dtick=0.1),
-                    bargap=0.05
-                )
-
-                st.plotly_chart(fig_corr_activity, use_container_width=True)
 
 if __name__ == "__main__":
     main()
